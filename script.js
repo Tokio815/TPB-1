083164a3fe2818aca5609ae6561eb1a9ac97c999
Tokio815 <tockey.cat@gmail.com>
2026-07-23 13:39:27 +0900

    Add team and player data to script.js土台追加

---
+++ script.js
@@ -0,0 +1,53 @@
+const TPB = {
+
+    season: "2026年度 第1シーズン",
+
+    teams: [
+
+        {
+
+            name: "トキトキホークス",
+
+            manager: "齋藤時生",
+
+            players: []
+
+        },
+
+        {
+
+            name: "フォレストブックス",
+
+            manager: "森本成名",
+
+            players: []
+
+        },
+
+        {
+
+            name: "佐内葵",
+
+            manager: "佐内葵",
+
+            players: [
+
+                {
+
+                    number: 6,
+
+                    name: "佐内葵",
+
+                    type: "投手・野手兼任"
+
+                }
+
+            ]
+
+        }
+
+    ]
+
+};
+
+console.log(TPB);
